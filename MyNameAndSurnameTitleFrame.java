d17e5ee0eec6d0b6e86e2f6d1a76712eab1fef51
Mncedisi Mthembu <139646291+221084403@users.noreply.github.com>
2023-07-16 08:26:52 +0200

    Add files via upload

---
+++ MyNameAndSurnameTitleFrame.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.ui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author MNCEDISI
+ */
+public class MyNameAndSurnameTitleFrame extends JFrame
+{
+    public MyNameAndSurnameTitleFrame() 
+    {
+        setTitle("Mncedisi Mthembu");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 550);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setVisible(true);
+    }
+    
+}
---
+++ MyNameAndSurnameTitleFrameApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mynameandsurnametitleframeapp;
+
+import za.ac.tut.ui.MyNameAndSurnameTitleFrame;
+
+/**
+ *
+ * @author MNCEDISI
+ */
+public class MyNameAndSurnameTitleFrameApp 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        MyNameAndSurnameTitleFrame frameApp = new MyNameAndSurnameTitleFrame();
+    }
+    
+}
